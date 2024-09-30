fe5ad50293990ff28c0d48e403810b16a9be1955
MD. SABBIR HOSEN IMON <151059564+imon93@users.noreply.github.com>
2024-10-01 00:58:24 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {});
+
+
+// SWIPER JS 
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    clickable: true,
+});
+
+
+
+
+// $(document).ready(function() {
+
+//     $(window).on('load', function() {
+//         $('.preloader_area').fadeOut(1000)
+//     });
+
+//     $('.back_to_top').click(function() {
+//         $('html, body').animate({scrollTop: 0}, 500)
+//     });
+
+//     $(window).scroll(function() {
+//         var scrolling = $(this).scrollTop()
+
+//         if (scrolling > 200) {
+//             $('.navbar').addClass('Color3')
+//             $('.back_to_top').fadeIn(500)
+//         }
+//         else{
+//             $('.navbar').removeClass('Color3')
+//             $('.back_to_top').fadeOut(500)
+//         }
+//     });
+
+//     $('.reserve_btn').click(function() {
+//         alert('Thank you! Your table has been booked.')
+//     });
+// });
