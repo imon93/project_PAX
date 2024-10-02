9679750db78eb9d963b8d82e3d713acdd5d37328
MD. SABBIR HOSEN IMON <151059564+imon93@users.noreply.github.com>
2024-10-02 21:22:14 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-$(document).ready(function() {});
+$(document).ready(function() {
 
+    $(window).on('load', function() {
+        $('.center').slideUp(1500)
+    });
+
+    $('.back_to_top').click(function() {
+        $('html, body').animate({scrollTop: 0}, 500)
+    });
+
+    $(window).scroll(function(){
+        var scrolling = $(this).scrollTop()
+
+        if (scrolling > 200) {
+            $('.navbar').addClass('third_color')
+            $('.back_to_top').fadeIn(500)
+        }
+        else{
+            $('.navbar').removeClass('third_color')
+            $('.back_to_top').fadeOut(500)
+        }
+    });
+});
 
 // SWIPER JS 
 
@@ -25,33 +46,6 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
 });
 
+// MIXITUP JS
 
-
-
-// $(document).ready(function() {
-
-//     $(window).on('load', function() {
-//         $('.preloader_area').fadeOut(1000)
-//     });
-
-//     $('.back_to_top').click(function() {
-//         $('html, body').animate({scrollTop: 0}, 500)
-//     });
-
-//     $(window).scroll(function() {
-//         var scrolling = $(this).scrollTop()
-
-//         if (scrolling > 200) {
-//             $('.navbar').addClass('Color3')
-//             $('.back_to_top').fadeIn(500)
-//         }
-//         else{
-//             $('.navbar').removeClass('Color3')
-//             $('.back_to_top').fadeOut(500)
-//         }
-//     });
-
-//     $('.reserve_btn').click(function() {
-//         alert('Thank you! Your table has been booked.')
-//     });
-// });
+var mixer = mixitup('.mixitup');
